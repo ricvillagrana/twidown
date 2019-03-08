d02fb63fdb6713de42c773cc372725094ff0eaed
Ricardo Villagrana <rvillagrana@protonmail.com>
2019-03-08 16:56:25 -0600

    imp: clean up job

--- app/jobs/follower_email_job.rb
+++ app/jobs/follower_email_job.rb
@@ -2,12 +2,8 @@ class FollowerEmailJob
   @queue = :follow_email_job
 
   def self.perform(following_id, follower_id)
-    Resque.logger = Logger.new("#{Rails.root}/log/resque.log")
-    Resque.logger.level = Logger::DEBUG
-
     following = User.find following_id
     follower  = User.find follower_id
-    Resque.logger.info "Sending email to #{following.name}[id: #{following.id}] because #{follower.name}[id: #{follower.id}] followed them."
 
     NewFollowerMailer.new_follower(following, follower).deliver
   end
